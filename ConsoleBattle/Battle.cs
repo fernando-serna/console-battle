c5f9d38cec5d747dc5457a058356016b891f3c46
Fernando Serna <fernando@Fernandos-MBP.hsd1.wa.comcast.net>
2019-07-03 01:10:29 -0700

    Initial commit

---
+++ ConsoleBattle/Battle.cs
@@ -0,0 +1,55 @@
+﻿using System;
+namespace ConsoleBattle
+{
+    public class Battle
+    {
+        // recieve both warrior objects
+        public static void StartFight(Warrior warrior1,
+            Warrior warrior2)
+        {
+            // loop giving each warrior chance to attack and block
+            while (true)
+            {
+                if (GetAttackResults(warrior1, warrior2) == "Game Over" || GetAttackResults(warrior2, warrior1) == "Game Over")
+                {
+                    Console.WriteLine("Game Over");
+                    break;
+                }
+            }
+        }
+
+        // accept 2 warriors
+        public static string GetAttackResults(Warrior warriorA,
+            Warrior warriorB)
+        {
+            // calculate one warriors attack and the others block
+            double warAAttkAmt = warriorA.Attack();
+            double warBBlkAmt = warriorB.Block();
+
+            // subtract block from attack
+            double dmg2WarB = warAAttkAmt - warBBlkAmt;
+
+            // if there was damage subtract that from the health
+            if (dmg2WarB > 0)
+            {
+                warriorB.Health -= dmg2WarB;
+            }
+            else dmg2WarB = 0;
+
+            // print out info on who attacked who and for how much
+            Console.WriteLine("{0} Attacks {1} and Deals {2} Damage",
+                warriorA.Name, warriorB.Name, dmg2WarB);
+
+            // provide output on the change to health
+            Console.WriteLine($"{warriorB.Name} Has {warriorB.Health}\n");
+
+            // check if the warriors health fell below zero and if so, end loop
+            if (warriorB.Health <= 0)
+            {
+                Console.WriteLine($"{warriorB.Name} has Died and {warriorA.Name} is Victorious\n");
+                return "Game Over";
+            }
+            else return "Fight Again";
+        }
+    }
+}
---
+++ ConsoleBattle/Warrior.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace ConsoleBattle
+{
+    public class Warrior
+    {
+        // define warrior properties
+        public string Name { get; set; } = "Warrior";
+        public double Health { get; set; } = 0;
+        public double AttkMax { get; set; } = 0;
+        public double BlockMax { get; set; } = 0;
+
+        // create and reuse single random instance else we'll get same value every time
+        Random rnd = new Random();
+
+        // constructor initializes warrior
+        public Warrior(string name = "Warrior",
+            double health = 0,
+            double attkMax = 0,
+            double blockMax =0)
+        {
+            Name = name;
+            Health = health;
+            AttkMax = attkMax;
+            BlockMax = blockMax;
+        }
+
+        // generate random attack value from 1 to warriors max attack value
+        public double Attack()
+        {
+            return rnd.Next(1, (int)AttkMax);
+        }
+
+        // generate random block value
+        public double Block()
+        {
+            return rnd.Next(1, (int)BlockMax);
+        }
+    }
+}
